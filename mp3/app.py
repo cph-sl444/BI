75085b4e917d2274376c8447b668e8c7716b5624
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2024-02-20 13:16:11 +0100

    update showing data in table

--- mp3/app.py
+++ mp3/app.py
@@ -3,5 +3,18 @@ import pandas as pd
 
 def load_data(file_path, label):
     # Load the Excel file, setting the second row (index 1) as the header
-    df = pd.read_excel(file_path, header=1)
+    df = pd.read_csv(file_path, header=1)
     return df
+
+def main():
+    st.title('Prediction by regression')
+    # Specify the file paths (adjust these paths if your files are in a different directory)
+    file_path = 'house-data.csv'
+    load_data(file_path, 'House Data')
+    st.write("Aggregated House Data:")
+    # display the aggregated DataFrame
+    st.dataframe(load_data(file_path, 'House Data'))
+    
+
+if __name__ == "__main__":
+    main()
